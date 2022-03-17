df77549428033ad7e55eec2f607e974669bc1e97
Gabbo-DEV <gabrieletroccoli90@gmail.com>
2022-03-17 15:58:59 +0100

    refactoring

--- receiver.py
+++ receiver.py
@@ -1,37 +1,46 @@
-import numpy
+from gettext import npgettext
 
-def calculate_receiver_position():
-    print("work_in_progress")
+import numpy as np
 
-def ComputeConvolution(array_one, array_two):
-    numpy.convolve(array_one, array_two)
 
-def ComputePower(input_signal):
-    #The power of a signal is the sum of the absolute squares of its time-domain samples divided 
-    #by the signal length, or, equivalently, the square of its RMS level.
-    
-    n_sample = input_signal.shape[0]
-    n_time = input_signal.shape[1]
-    
-    results_array = np.empty((n_sample, 1))
-    
-    for i in range(n_sample):
-        sum_sample = 0
-        for j in range(n_time):
-            sum_sample += input_signal[i, j]*input_signal[i, j]
-        sum_sample = sum_sample/n_time
-        results_array[i] = sum_sample
-    
-    return results_array
+class Receiver:
+    def __init__(self):
+        pass
+
+    def calculate_receiver_position():
+        pass
+
+    def ComputeConvolution(array_one, array_two):
+        np.convolve(array_one, array_two, 'valid')
+
+    def ComputePower(input_signal):
+        # The power of a signal is the sum of the absolute squares of its time-domain samples divided
+        # by the signal length, or, equivalently, the square of its RMS level.
+
+        n_sample = input_signal.shape[0]
+        n_time = input_signal.shape[1]
+
+        results_array = npgettext.empty((n_sample, 1))
+
+        for i in range(n_sample):
+            sum_sample = 0
+            for j in range(n_time):
+                sum_sample += input_signal[i, j]*input_signal[i, j]
+            sum_sample = sum_sample/n_time
+            results_array[i] = sum_sample
+
+        return results_array
+
 
 def main():
-    InitializeParams()
-    outs = ComputeConvolution(input)
-    powers = ComputePowers(outs)
-    positions = ComputePosition(powers)
-    print("outs='" + outs + "'")
-    print("powers='" + powers + "'")
-    print("positions='" + positions + "'")
+    receiver = Receiver()
+    # InitializeParams()
+    outs = receiver.ComputeConvolution(input)
+    powers = receiver.ComputePowers(outs)
+    positions = receiver.ComputePosition(powers)
+    print(f"outs= ${outs}")
+    print(f"powers= ${powers}")
+    print(f"positions= ${positions}")
 
 
 if __name__ == "__main__":
